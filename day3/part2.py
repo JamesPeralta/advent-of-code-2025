6fca196cffecee84e67e44dfacb61d4235746d6a
James Peralta <jamesperalta.swe@gmail.com>
2025-12-05 22:41:57 -0800

    part 2 working?

--- day3/part2.py
+++ day3/part2.py
@@ -1,26 +1,33 @@
-f = open("test.txt", "r")   # open for reading
+f = open("actual.txt", "r")   # open for reading
 input_str = f.read()
 f.close()
 
-def back_track(input_str, pointer, curr_arr, res=[]):
-    if len(curr_arr) == 12:
-        res.append(int("".join(curr_arr)))
-        return
-    
-    for i in range(pointer, len(input_str)):
-        curr_arr.append(input_str[i])
-        back_track(input_str, i + 1, curr_arr, res)
-        curr_arr.pop()
 
-    return
+def find_largest(input_str):
+    arr = []
+    index = 0
+    num_left = 12
+    while num_left > 0:
+        left_part = input_str[index:len(input_str) - num_left + 1]
+        max_res = max(left_part)
+        index = index + left_part.find(max_res) + 1
+        arr.append(max_res)
+        num_left -= 1
+
+    return int("".join(arr))
+    
+
 
 input_str = input_str.split("\n")
 result = 0
 for line in input_str:
-    res = []
-    back_track(line, 0, [], res)
-    max_res = max(res)
-
-    result += max_res
+    result += find_largest(line)
 
 print(result)
+
+"""
+987654321111
+811111111119
+434234234278
+888911112111 = 3121910778619.
+"""
