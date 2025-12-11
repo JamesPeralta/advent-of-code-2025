e6fb7da91777642c78ce50898698815d3cddbff7
James Peralta <jamesperalta.swe@gmail.com>
2025-12-10 22:03:45 -0800

    part2 done

--- day11/part2.py
+++ day11/part2.py
@@ -15,14 +15,24 @@ for line in input_str:
 
 # print(input_str)
 
-def dfs(node, path, adj_list):
+def dfs(node, seen_dac, seen_fft, adj_list, cache):
     if node == "out":
-        return 1
+        if seen_dac and seen_fft:
+            return 1
+        return 0
+
+    if (node, seen_dac, seen_fft) in cache:
+        return cache[(node, seen_dac, seen_fft)]
+
+    is_fft = node == "fft"
+    is_dac = node == "dac"
 
     result = 0
     for neighb in adj_list[node]:
-        dfs(neighb, path, adj_list)
+        result += dfs(neighb, seen_dac or is_dac, seen_fft or is_fft, adj_list, cache)
 
+    cache[(node, seen_dac, seen_fft)] = result
     return result
 
-print(dfs("you", [], adj_list))
+print(adj_list)
+print(dfs("svr", False, False, adj_list, {}))
